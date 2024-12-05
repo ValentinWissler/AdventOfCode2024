2211fd282f3f48e5a3d6ac0e54352f78aae10a21
Valentin Wissler <valentin.wissler@n-able.com>
2024-12-05 21:22:38 +0000

    day4

---
+++ day4/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const (
+	INPUT_FILE = "./crossword.txt" // Puzzle input
+	TEST_FILE  = "./mini.txt"      // To test if the code is working, the mini contains 18 XMAS
+)
+
+var Directions = [][]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}}
+
+func main() {
+	puzzle, err := ReadFile(INPUT_FILE)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	c := SearchPuzzle(puzzle, "XMAS")
+	fmt.Println("The count of XMAS is ", c)
+	c2 := SearchPuzzleV2(puzzle)
+	fmt.Println("The count of X-MAS is ", c2)
+}
+
+// Return how many times word appears in input, see Directions for possible moves
+func SearchPuzzle(input, word string) int {
+	puzzle := strings.Split(input, "\n")
+	for x, p := range puzzle {
+		puzzle[x] = strings.TrimSpace(p)
+	}
+	maxH, maxV := len(puzzle[0]), len(puzzle)
+	count := 0
+	for rowIndex := 0; rowIndex < maxV; rowIndex++ {
+		for colIndex := 0; colIndex < maxH; colIndex++ {
+			count += FindWordAt(puzzle, word, rowIndex, colIndex)
+		}
+	}
+	return count
+}
+
+func SearchPuzzleV2(input string) int {
+	puzzle := strings.Split(input, "\n")
+	for x, p := range puzzle {
+		puzzle[x] = strings.TrimSpace(p)
+	}
+	maxH, maxV := len(puzzle[0]), len(puzzle)
+	count := 0
+	for rowIndex := 0; rowIndex < maxV; rowIndex++ {
+		for colIndex := 0; colIndex < maxH; colIndex++ {
+			// Search diagonaly for MAS or SAM
+			if puzzle[rowIndex][colIndex] == 'A' {
+				tlr, tlc := rowIndex-1, colIndex-1
+				if !(tlr >= 0 && tlr < maxV) || !(tlc >= 0 && tlc < maxH) {
+					continue
+				}
+				trr, trc := rowIndex-1, colIndex+1
+				if !(trr >= 0 && trr < maxV) || !(trc >= 0 && trc < maxH) {
+					continue
+				}
+				brr, brc := rowIndex+1, colIndex+1
+				if !(brr >= 0 && brr < maxV) || !(brc >= 0 && brc < maxH) {
+					continue
+				}
+				blr, blc := rowIndex+1, colIndex-1
+				if !(blr >= 0 && blr < maxV) || !(blc >= 0 && blc < maxH) {
+					continue
+				}
+
+				// Now that the moves are in bound, see if we can find the word
+				if (puzzle[tlr][tlc] == 'M' && puzzle[brr][brc] == 'S') || (puzzle[tlr][tlc] == 'S' && puzzle[brr][brc] == 'M') {
+					if (puzzle[trr][trc] == 'S' && puzzle[blr][blc] == 'M') || (puzzle[trr][trc] == 'M' && puzzle[blr][blc] == 'S') {
+						count++
+					}
+				}
+			}
+		}
+	}
+	return count
+}
+
+func FindWordAt(puzzle []string, word string, begCi, begRi int) int {
+	maxH, maxV := len(puzzle[0]), len(puzzle)
+	count := 0
+	// Check if moving in a direction is within bound
+	for _, dir := range Directions {
+		found := ""
+		c, r := begCi, begRi
+		for i := 0; i < len(word); i++ {
+			// If the move is within bounds
+			if (c >= 0 && c < maxV) && (r >= 0 && r < maxH) {
+				found += string(puzzle[c][r])
+			} else {
+				break
+			}
+			// Move cursor
+			c, r = c+dir[0], r+dir[1]
+		}
+		if found == word {
+			count++
+		}
+	}
+	return count
+}
+
+// Return the content of a file as a string
+func ReadFile(file string) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	content, err := io.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	return string(content), nil
+}
